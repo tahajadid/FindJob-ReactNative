508c1e30aedd1f721e874592bcce0cb5b0a0f85b
Taha Jadid <jadid.taha@waroom.com>
2024-02-19 20:32:57 +0100

    Chnage style

--- App/UI/LoginUI/Login.jsx
+++ App/UI/LoginUI/Login.jsx
@@ -1,10 +1,19 @@
-import { View, Text } from 'react-native'
+import { View, Text, Image, StyleSheet } from 'react-native'
 import React from 'react'
 
 export default function Login() {
   return (
     <View>
-      <Text>Login</Text>
+      <Image source={require('../../../assets/icon.png')} 
+              style={styles.loginImage}
+      />
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  loginImage:{
+    width:230,
+    height:450
+  }
+})
